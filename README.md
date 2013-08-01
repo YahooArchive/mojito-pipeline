c6207713fa3274e75d76a256b709487d6593bd83
arnoux <arnoux@yahoo-inc.com>
2013-08-01 23:37:04 +0000

    skeleton classes

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 mojito-pipeline
 ===============
+
+##Task
+### dependencies - members
+	* children: ["child1", "child2"] - synchronous children
+	* renderWith: ["asyncChild1", "asyncChild2"] - async children
+	* displayAfter: ["head", "body"] - client side display control
+
+### fire: states/events – when to fire / functions must call this.fire('someEvent')
+	* error: dispatchError(pipeline) {Function} {default: on error from one of renderWith or if exception}
+	* timeout: dispatchTimeout(pipeline)|100 [Function|Number] {default: on timeout from one of renderWith or if timeout}
+	* rendered: dispatchRendered(pipeline) {Function} {default: after render}
+	* flushed: dispatchFlushed(pipeline) {Function} {default: after flushed}
+	* displayed: dispatchDisplayed(clientPipeline) {Function} {default: after display / on show}
+
+### targets: tasks - what event sources to subscribe to / '*' would be the pipeline/all targets
+	* error: ["importantTask1", "importantTask2"] {Array} {default: the renderWith config}
+	* timeout: ["importantTask1", "importantTask2"] {Array} {default: the renderWith config}
+	* rendered: ["child1", "child2"] {Array} {default: the renderWith config}
+	* flushed: "body" {String} {default: the flushAfter config}
+	* displayed: ["header", "body"] {Array} {default: the displayAfter config}
+
+
+	"master": {
+		"type": "Master",
+		"config": {
+			""
+		}
+	}
+
+	Task
+		footerChild.on('render', function () {
+			this.render(function () {
+				body.on('flushed', function() {
+					this.flush();
+				});
+			}
+		});
+
+
+	Master
+		for (acticle in articles)
+			pipeline.push(new Task(cfg.get(article.type).mojitoConfig.task));
+
--- index.js
+++ index.js
@@ -5,17 +5,101 @@
 YUI.add('mojito-pipeline-addon', function (Y, NAME) {
 	'use strict';
 
-	function PipelineAddon() {
-		PipelineAddon.superclass.constructor.apply(this, arguments);
+	/**
+	 * The main task processor - takes care of the lifecycle
+	 * of the tasks
+	 * @param {Object} command the command from the action context
+	 * @param {Object} adapter the output adapter for flushing the rendered views
+	 * @param {ActionContext} ac the action context for the current request
+	 */
+
+	function Pipeline(command, adapter, ac) {
+		Pipeline.superclass.constructor.apply(this, {
+			command: command,
+			adapter: adapter,
+			ac: ac
+		});
+		this.Task.prototype.pipeline = this;
+
+		this.on('rendered', dispatchRender);
 	}
-	PipelineAddon.NS = 'pipeline';
 
-	Y.extend(PipelineAddon, Y.Plugin.Base, {
-		push: function (task) {
-
+	Y.extend(Pipeline, Y.Base, {
+		namespace: 'pipeline',
+		initializer: function (config) {
+			this.sections = config.sections;
+			this.rules = {};
+			this._resolveRules();
+			this._flushQueue = [];
 		}
 	});
+	Pipeline.prototype._resolveRules = function() {
+		// this.rules = ...
+	};
+	Pipeline.prototype.render = function(task) {
+	};
+	Pipeline.prototype.flushQueue = function() {
+	};
+	Pipeline.prototype.getTask = function(id) {
+	};
+	Pipeline.prototype.push = function(task) {
+		var nextChild,
+			childrenTest = function () {
+				var child,
+					test = true;
+				for (child in task.children) {
+					test = test & pipeline.getTask(nextChild).rendered;
+				}
+			},
+			combinedTest = pipeline.combineTests(task.renderTest, childrenTest),
+			renderAction = function () {
+				if (combinedTest()) {
+					pipeline.render(task);
+				}
+			};
 
-	Y.namespace('mojito.addons.ac');
-	Y.mojito.addons.ac.pipeline = PipelineAddon;
-}, '0.0.1', { requires: ['mojito-composite-addon']});
+		for (nextChild in task.children) {
+			this.rules[nextChild] = this.rules[nextChild] || {};
+			this.rules[nextChild].render = this.rules[nextChild].render || [];
+			task.renderTest = pipeline._combineTests(task.renderTest, );
+			this.rules[nextChild].render.push();
+		}
+	};
+	Pipeline.prototype.close = function() {
+	};
+	Pipeline.prototype.setRule = function(subscriptor, method, config) {
+	};
+	Pipeline.prototype._combineTests = function (test1, test2) {
+		return function () {
+			return test1() && test2();
+		};
+	};
+
+	function Task() {
+		Task.superclass.constructor.apply(this, arguments);
+	}
+	Y.extend(Task, Y.Base, {
+		initialize: function (options) {
+			this.config = options.config;
+			this.id = options.id;
+			this.action = options.action;
+			this.children = options.children;
+		}
+	});
+	Task.prototype.render = function() {
+		// this.pipeline.render(this);
+	};
+	Task.prototype.flush = function() {
+		// this.prototype.flush();
+	};
+
+	Task.prototype.renderTest = Task.prototype.flushTest = function() {
+		return true;
+	};
+
+	Pipeline.prototype.Task = Task;
+	Pipeline.NAME = 'pipeline';
+	Y.namespace('mojito.addons.ac').pipeline = Pipeline;
+}, '0.0.1', {
+	requires: ['base-base']
+});
