0c80234235079ca60d85834098ad08d3eb9ae0e9
jimenez <jimenez@yahoo-inc.com>
2013-08-06 18:10:47 -0700

    Functional addon.

--- addons/ac/pipeline.server.js
+++ addons/ac/pipeline.server.js
@@ -15,13 +15,6 @@ YUI.add('mojito-pipeline-addon', function (Y, NAME) {
      */
 
     function Pipeline(command, adapter, ac) {
-
-        /*Pipeline.superclass.constructor.apply(this, {
-            command: command,
-            adapter: adapter,
-            ac: ac
-        });*/
-
         this.command = command;
         this.adapter = adapter;
         this.ac = ac;
@@ -30,10 +23,76 @@ YUI.add('mojito-pipeline-addon', function (Y, NAME) {
         this.tasks = {};
         this.closed = false;
         this.client = new Pipeline.Client();
-        this.numPushedTasks = 0;
+        this.unprocessedTasks = 0;
         this._flushQueue = [];
-        Task.prototype.pipeline = this;
     }
+
+    function Task(task) {
+        var self = this;
+        Y.mix(this, task, true);
+
+        // get children tasks
+        // and determine render targets
+        this.children = {};
+        this.renderTargets = {};
+        Y.Array.each(task.dependencies, function (dependency) {
+            self.children[dependency] = self.pipeline.getTask(dependency);
+            self.renderTargets[dependency] = ['afterRender'];
+        });
+
+        // by default the flush test has one target (the task's render event itself)
+        this.flushTargets = {};
+        this.flushTargets[this.id] = ['afterRender'];
+    }
+
+    Task.EVENT_TYPES = ['beforeRender', 'afterRender'];
+
+    Task._combineTests = function () {
+        return function () {
+            return !Y.Array.some(arguments, function (nextFn) {
+                return !nextFn.call();
+            });
+        };
+    };
+
+    Task.prototype = {
+        render: function (done) {
+            var pipeline = this.pipeline,
+                task = this;
+            pipeline.events.fire(task.id, 'beforeRender', function () {
+                var command = {
+                        instance: task,
+                        action: task.action || 'index',
+                        context: pipeline.command.context,
+                        params: task.params || pipeline.command.params
+                    },
+                    adapter = new Pipeline.Adapter(task, pipeline.adapter, function (data, meta) {
+                        var subscription;
+                        task.data = data;
+                        task.meta = meta;
+
+                        // fire after render event
+                        pipeline.events.fire(task.id, 'afterRender', function () {
+                            done(data, meta);
+                        }, data, meta);
+                    });
+
+                pipeline.ac._dispatch(command, adapter);
+            }, task);
+        },
+
+        addToFlushQueue: function () {
+            this.pipeline._flushQueue.push(this);
+        },
+
+        renderTest: function () {
+            return !Y.Object.some(this.children, function (task) {
+                return !task.rendered;
+            });
+        },
+
+        flushTest: function () { return this.rendered; }
+    };
 
     Pipeline.EVENT_TYPES = ['beforeFlush', 'afterFlush'];
 
@@ -67,14 +126,8 @@ YUI.add('mojito-pipeline-addon', function (Y, NAME) {
         }
     };
 
-
-
     Pipeline.prototype = {
         namespace: 'pipeline',
-
-        getTask: function (id) {
-
-        },
 
         on: function (targetAction, action) {
             this.events({
@@ -90,36 +143,6 @@ YUI.add('mojito-pipeline-addon', function (Y, NAME) {
                 pipeline.closed = true;
                 pipeline.flushQueue();
             });
-        },
-
-        render: function (task, done) {
-            var pipeline = this;
-            this.events.fire(task.id, 'beforeRender', function () {
-                var command = {
-                    instance: task,
-                    action: task.action || 'index',
-                    context: pipeline.command.context,
-                    params: task.params || pipeline.command.params
-                },
-                adapter = new Pipeline.Adapter(task, pipeline.adapter, function (data, meta) {
-                    var subscription;
-                    task.data = data;
-                    task.meta = meta;
-
-                    // fire after render event
-                    pipeline.events.fire(task.id, 'afterRender', function () {
-                        done(data, meta);
-                    }, data, meta);
-                });
-
-                pipeline.ac._dispatch(command, adapter);
-            }, task);
-
-            // TODO: create action context
-            // TODO: execute mojit
-            // TODO: execute any actions subscribe to this task's render event
-            // TODO: test flush condition, if true put in flush queue, else subscribe to events
-
         },
 
         flushQueue: function () {
@@ -143,222 +166,97 @@ YUI.add('mojito-pipeline-addon', function (Y, NAME) {
             }
         },
 
-        getTask: function (id) {},
+        getTask: function (config) {
+            var task;
+            // if getting task by id get task if it exists,
+            // if it doesn't exist create a dummy task
+            if (typeof config === 'string') {
+                task = this.tasks[config] = new Task({});
+                return task;
+            }
+
+            // if getting task with a configuration
+            // get task if it exists
+            // if it exits then merge the config
+            // else just create the task
+            task = this.tasks[config.id];
+            if (task) {
+                Y.mix(task, config, true);
+            } else {
+                task = this.tasks[config.id] = new Task(config);
+            }
+            return task;
+        },
 
         push: function (taskConfig) {
             var pipeline = this,
-                renderRuleTargets = {},
-                flushRuleTargets = {},
-                task = pipeline.getTask(taskConfig.id);
+                renderSubscription,
+                flushSubscription,
+                task = pipeline.getTask(taskConfig);
+
+            task.pushed = true;
+            task.pipeline = this;
 
             // keep track to know when to flush the batch
-            this.unrenderedTasks++;
+            this.unprocessedTasks++;
+
+            // subscribe to any events specified by the task
+            Y.Array.each(Task.EVENT_TYPES, function (targetAction) {
+                if (!task[targetAction]) {
+                    return;
+                }
+                var targets = {};
+                targets[task.id] = [targetAction];
+                pipeline.events.subscribe(targets, task[targetAction]);
+            });
 
             // for each section in the config
             Y.Object.each(task.sections, function (config, sectionId) {
-
                 // push the default sections if they're not already there
                 if (config['default']) {
-                    this.push(taskConfig);
+                    pipeline.push(config);
                 }
-                // build the targets for the render rule of the task
-                renderRuleTargets[taskConfig.id] = ['render'];
-            }, this);
+            });
 
             // TODO: parse the render rules and combine the tests with sectionsRenderTest
-            task.renderTest = Task._combineTests(task.renderTest/* here */);
+            task.renderTest = Task._combineTests(task.renderTest);
+            task.flushTest = Task._combineTests(task.flushTest);
 
-            // subscribe to the events triggering the render action under some condition
-            this.attachAction('render', renderRuleTargets, task.renderTest, function (data, meta) {
-                this.attachAction('addToFlushQueue', flushRuleTargets, task.flushTest);
-
-                pipeline.unrenderedTasks--;
-                if (pipeline.unrenderedTasks === 0) {
-                    // if so, empty the queue
-                    pipeline.flushQueue();
+            // subscribe to flush events
+            flushSubscription = this.events.subscribe(task.flushTargets, function (event) {
+                if (task.flushTest()) {
+                    // remove subscribed events such that this action doesn't get called again
+                    flushSubscription.unsubscribe();
+                    task.addToFlushQueue();
                 }
             });
+
+            if (task.renderTest()) {
+                task.render(function () {
+                    if (--pipeline.unprocessedTasks === 0) {
+                        // if so, empty the queue
+                        pipeline.flushQueue();
+                    }
+                });
+                return;
+            }
+
+            renderSubscription = this.events.subscribe(task.renderTargets, function (event) {
+                if (task.renderTest()) {
+                    // remove subscribed events such that this action doesn't get called again
+                    renderSubscription.unsubscribe();
+                    task.render();
+                }
+            });
+
+            if (--pipeline.unprocessedTasks === 0) {
+                // if so, empty the queue
+                pipeline.flushQueue();
+            }
 
             return;
-            var pipeline = this,
-                i,
-                targets,
-                targetAction;
-
-            // increment number of pushed tasks
-            // once all the pushed tasks have been handled, we need to flush anything in the flush queue
-            this.numPushedTasks++;
-
-            // subscribe to any events specified by the task
-            for (i = 0; i < Pipeline.prototype.Task.EVENT_TYPES.length; i++) {
-                targetAction = Pipeline.prototype.Task.EVENT_TYPES[i];
-                if (!task[targetAction]) {
-                    continue;
-                }
-                targets = {};
-                targets[task.id] = [targetAction];
-                this.events.subscribe(targets, task[targetAction]);
-            }
-
-            // TODO: create tasks for any default child section of this task
-
-            // release control in order to make this method asynchronous
-            process.nextTick(function () {
-
-                var child,
-                    originalTest = task.renderTest,
-                    childrenTest,
-                    eventTargets = {},
-                    subscription;
-
-                // TODO: get the event targets for the original renderTest
-                // this is important in order to subscribe to events if necessary
-                /*
-                    ex.
-                    eventTargets = this.rules[task.section].targets
-                 */
-
-                // if task has children then create a combination of the renderTest
-                if (task.children) {
-                    // checks if all the children have been rendered
-                    childrenTest = function () {
-                        var child;
-                        for (child in task.children) {
-                            if (!pipeline.getTask(child).rendered) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    };
-
-                    // subscribe to the events
-                    for (child in task.children) {
-                        eventTargets[child] = ['afterRender'];
-                    }
-
-                    // replace task's renderTest method with the combined childrenTest
-                    // and the original renderTest
-                    task.renderTest = function () {
-                        return originalTest() && childrenTest();
-                    };
-                }
-
-                // test render condition
-                if (task.renderTest()) {
-                    pipeline.render(task, function () {
-                        pipeline.ac.done();
-                        if (--pipeline.numPushedTasks === 0) {
-                            pipeline.flushQueue();
-                        }
-                    });
-                    return;
-                }
-
-                // render condition is false so now need to subscribe to events
-
-                // it is important that any subscription for the sakes of rendering or flushing
-                // should be done only once per task/action otherwise a task may have multiple subscribed events
-                // trying to render or flush for the same target action
-                subscription = this.events.subscribe(eventTargets, function (event) {
-                    if (task.renderTest()) {
-                        // remove subscribed events such that this action doesn't get called again
-                        subscription.unsubscribe();
-                        pipeline.render(task);
-                    }
-                });
-
-                if (--this.numPushedTasks === 0) {
-                    this.flushQueue();
-                }
-            });
         }
     };
-
-
-    function Task(task) {
-        Y.mix(this, task, true);
-        //Task.superclass.constructor.apply(this, arguments);
-    }
-
-    Task.EVENT_TYPES = ['beforeRender', 'afterRender'];
-
-    Task._combineTests = function () {
-        return function () {
-            return !Y.Array.some(arguments, function (nextFn) {
-                return !nextFn.call();
-            });
-        };
-    };
-
-    Task.prototype = {
-        render: function (task, done) {
-            var pipeline = this;
-            this.events.fire(task.id, 'beforeRender', function () {
-                var command = {
-                    instance: task,
-                    action: task.action || 'index',
-                    context: pipeline.command.context,
-                    params: task.params || pipeline.command.params
-                },
-                adapter = new Pipeline.Adapter(task, pipeline.adapter, function (data, meta) {
-                    var subscription;
-                    task.data = data;
-                    task.meta = meta;
-
-                    // fire after render event
-                    pipeline.events.fire(task.id, 'afterRender', function () {
-                        done(data, meta);
-                    }, data, meta);
-                });
-
-                pipeline.ac._dispatch(command, adapter);
-            }, task);
-        },
-
-        addToFlushQueue: function () {
-            this.pipeline._flushQueue.push(this);
-        },
-
-        attachAction: function (action, targets, testFn, afterAction) {
-            var subscription;
-            afterAction = afterAction || function () {};
-
-            if (testFn()) {
-                this[action].call(this, afterAction);
-            } else {
-                subscription = this.pipeline.events.subscribe(targets, function (e, done) {
-                    if (testFn()) {
-                        subscription.unsubscribe();
-                        this[action].call(this, function () {
-                            afterAction();
-                            done();
-                        });
-                    }
-                });
-            }
-        },
-
-        renderTest: function () {
-            return !Y.Object.some(this.children, function (task) {
-                return !task.rendered;
-            });
-        },
-
-        flushTest: function () {return true;}
-    };
-
-    Pipeline.prototype.Task = Task;
-
-    /*Y.extend(Pipeline, Y.Base, {
-        initializer: function () {
-        }
-    });*/
-
-    /*Y.extend(Task, Y.Base, {
-        initialize: function (options) {
-
-        }
-    });*/
 
     Y.namespace('mojito.addons.ac').pipeline = Pipeline;
 }, '0.0.1', {
