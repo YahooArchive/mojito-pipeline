d5cbc00cee8ad876ef9915f47856ebf660a6ef8a
jimenez <jimenez@yahoo-inc.com>
2013-08-07 21:40:56 -0700

    Full sample app.

--- apps/app1/mojits/Master/controller.server.js
+++ apps/app1/mojits/Master/controller.server.js
@@ -1,14 +1,63 @@
+/*jslint nomen: true, plusplus: true */
 YUI.add('MasterController', function (Y, NAME) {
     'use strict';
     Y.namespace('mojito.controllers')[NAME] = {
         index: function (ac) {
             ac.assets.addCss('/static/app/assets/base.css');
             ac.assets.addCss('./master.css');
-            ac.done();
+
+            var i,
+                id,
+                children,
+                searchResultsDependencies = [];
+
+            // push search-results children
+            for (i = 1; i <= 6; i++) {
+                id = 'search-result' + i;
+                searchResultsDependencies.push(id);
+                ac.pipeline.push({
+                    id: id,
+                    group: 'results',
+                    type: 'Box',
+                    config: {
+                        title: id,
+                        taskType: 'dependency'
+                    }
+                });
+            }
+
+            // push sections
+            Y.Object.each(ac.pipeline._sections, function (section) {
+                if (section.sectionName === 'root') {
+                    return;
+                }
+                ac.pipeline.push({
+                    id: section.sectionName,
+                    type: section.type,
+                    dependencies: section.sectionName === 'search-results' ? searchResultsDependencies : [],
+                    config: {
+                        title: section.sectionName,
+                        taskType: section['default'] ? 'default section' : 'section'
+                    }
+                });
+            });
+
+            ac.pipeline.close();
+
+            children = ac.params.body('children');
+            if (ac.pipeline.client.jsEnabled) {
+                ac.done(children);
+            } else {
+                ac.pipeline.on('close', function () {
+                    ac.done(children);
+                });
+            }
         }
     };
 }, '0.0.1', {
     requires: [
-        'mojito-assets-addon'
+        'mojito-pipeline-addon',
+        'mojito-assets-addon',
+        'mojito-params-addon'
     ]
 });
