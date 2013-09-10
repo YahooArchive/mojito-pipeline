75c7f42564d36777d4fec58000eadecd22128752
arnoux <arnoux@yahoo-inc.com>
2013-09-10 21:40:04 +0000

    added more copyright notices

--- mojits/PipelineHTMLFrameMojit/controller.server.js
+++ mojits/PipelineHTMLFrameMojit/controller.server.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 /*jslint anon:true, sloppy:true, nomen:true*/
 /*global YUI*/
 YUI.add('PipelineHTMLFrameMojit', function (Y, NAME) {
